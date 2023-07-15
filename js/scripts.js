ad59bda9a779f42b2dff93112bbe437edc477448
Debo <dwdeante0726@gmail.com>
2023-07-15 16:28:46 -0700

    This will use variables from forms in function, to create a new object.

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,17 @@ window.addEventListener("load", function() {
     this.t3 = topping3;
   }
 
-  
-})
+  var form = document.forms["pizzaform"];
+        var sizeInput = form.elements["size"];
+        var t1Input = form.elements["t1"];
+        var t2Input = form.elements["t2"];
+        var t3Input = form.elements["t3"];
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var size = sizeInnput.value;
+    var topping1 = t1Input.value;
+    var topping2 = t2Input.value;
+    var topping3 = t3Input.value;
+  });
+});
