8d7158e843ef49c8037c8ec6f7295d7d0c07d24a
Debo <dwdeante0726@gmail.com>
2023-07-15 11:10:15 -0700

    This will add more objects for toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 window.addEventListener("load", function() {
-  function Pizza(size) {
+  function Pizza(size, topping1, topping2, topping3) {
     this.pizzaSize = size;
+    this.t1 = topping1;
+    this.t2 = topping2;
+    this.t3 = topping3;
   }
 })
