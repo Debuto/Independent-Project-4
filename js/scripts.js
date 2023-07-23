19b809ab3014c841af2796ae195ea46f6f3842c9
Debo <dwdeante0726@gmail.com>
2023-07-22 21:10:52 -0700

    This will add test run, failed unable to find script.js in test.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,12 @@
-window.addEventListener("load", function() {
-  function Pizza(size, topping1, topping2, topping3) {
-    this.pizzaSize = size;
-    this.t1 = topping1;
-    this.t2 = topping2;
-    this.t3 = topping3;
-  }
+function Pizza(size, topping1, topping2, topping3) {
+  this.pizzaSize = size;
+  this.t1 = topping1;
+  this.t2 = topping2;
+  this.t3 = topping3;
+}
 
-  var form = document.forms["pizzaform"];
-    var sizeInput = form.elements["size"];
-    var t1Input = form.elements["t1"];
-    var t2Input = form.elements["t2"];
-    var t3Input = form.elements["t3"];
+Pizza.prototype.getPizzaDetails = function() {
+  return "So you ordered the " + this.pizzaSize + ", with the toppings: " + this.t1 + ", " + this.t2 + ", " + this.t3;
+};
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    var size = sizeInnput.value;
-    var topping1 = t1Input.value;
-    var topping2 = t2Input.value;
-    var topping3 = t3Input.value;
-  });
-})
+module.exports = Pizza;
---
+++ pizza.test.js
@@ -0,0 +1,16 @@
+const Pizza = require('./scripts');
+
+test('Creating a new Pizza object', () => {
+  const pizza = new Pizza('Large', 'Pepperoni', 'Mushrooms', 'Olives');
+
+  expect(pizza.pizzaSize).toBe('Large');
+  expect(pizza.t1).toBe('Pepperoni');
+  expect(pizza.t2).toBe('Mushrooms');
+  expect(pizza.t3).toBe('Olives');
+});
+
+test('Pizza.getPizzaDetails()', () => {
+  const pizza = new Pizza('Medium', 'Cheese', 'Ham', 'Pineapple');
+
+  expect(pizza.getPizzaDetails()).toBe('Size: Medium, Toppings: Cheese, Ham, Pineapple');
+});
